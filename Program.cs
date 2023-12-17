18affb6c956cb5e5cf7c039706736cb31e6dd009
Melanie <melaine.santos@ba.estudante.senai.br>
2023-12-17 20:40:54 -0300

    Fix all

---
+++ Program.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using MoviesPilgrim.Data;
+using Microsoft.Extensions.DependencyInjection;
+using MoviesPilgrim.Repository;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+string mySqlConnection = builder.Configuration.GetConnectionString("DefaultDatabase");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseMySql(mySqlConnection, ServerVersion.AutoDetect(mySqlConnection) ));
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddScoped<ILocadoraRepository, LocadoraRepository>();
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+app.Run();
---
+++ Repository/LocadoraRepository.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MoviesPilgrim.Data;
+using MoviesPilgrim.Models;
+
+namespace MoviesPilgrim.Repository
+{
+    public class LocadoraRepository : ILocadoraRepository
+    {
+        private readonly ApplicationDbContext application_DbContext;
+        public LocadoraRepository(ApplicationDbContext applicationDbContext){
+            application_DbContext = applicationDbContext;
+        }
+        public FilmeModel adicionar(FilmeModel filme)
+        {
+            application_DbContext.Filmes.Add(filme);
+            application_DbContext.SaveChanges();
+            return filme;
+        }
+
+        public List<FilmeModel> listarFilmes(){
+            return application_DbContext.Filmes.ToList();
+        }
+        public FilmeModel buscarId(int id){
+            return application_DbContext.Filmes.FirstOrDefault(x => x.Id == id);
+        }
+        public FilmeModel atualizar(FilmeModel filme){
+            FilmeModel filmeDB = buscarId(filme.Id);
+
+            if(filmeDB == null) throw new Exception("Filme não foi encontrado");
+
+            filmeDB.titulo = filme.titulo;
+            filmeDB.sinopse = filme.sinopse;
+            filmeDB.quantidade = filme.quantidade;
+            filmeDB.valor_filme = filme.valor_filme;
+            filmeDB.taxa_dia = filme.taxa_dia;
+            filmeDB.classificacao = filme.classificacao;
+            filmeDB.genero = filme.genero;
+
+            application_DbContext.Filmes.Update(filmeDB);
+            application_DbContext.SaveChanges();
+            return filmeDB;
+        }
+
+        public bool deletar(int id){
+            FilmeModel filmeDB = buscarId(id);
+
+            if(filmeDB == null) throw new Exception("Filme não foi encontrado");
+
+            application_DbContext.Filmes.Remove(filmeDB);
+            application_DbContext.SaveChanges();
+            return true;
+        }
+    
+    public List<ViewLocacoes> ListarLocacoes(){
+            return application_DbContext.ViewLocacoes.ToList();
+    }
+    
+    }
+}
